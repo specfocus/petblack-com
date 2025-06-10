7d60a4106a491b589b3b390c137b2dae7b444a0e
Lucas M. Oromi <lucas.oromi@gmail.com>
2025-06-10 09:46:58 -0600

    more pets

--- petblack/src/app/page.tsx
+++ petblack/src/app/page.tsx
@@ -1,16 +1,25 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+const images = [
+    {alt: 'dog', src: '/istockphoto-1188966913-612x612.jpg', height: 612, width: 612},
+    // {alt: 'bird', src: '/photo-1709439681846-60a076f48892.jpg', height: 4499, width: 3000},
+    {alt: 'cat', src: '/blackcat_Lifeonwhite_Alamy_Stock_Photo.jpeg', height: 855, width: 656},
+    {alt: 'fish', src: '/117919784-black-goldfish-isolated-on-background.jpg', height: 450, width: 450},
+    // {alt: 'dog and cat', src: '/360_F_316422277_s4yn3aOAojJnQPZE1SfKOFIPvSKqCxjv.jpg', height: 360, width: 548}
+];
+
+const index = (Date.now() >> Math.round((3 * Math.random()))) % images.length;
+const props = images[index];
+console.log(index, props);
+
 export default function Home() {
     return (
         <div className={styles.page}>
             <main className={styles.main}>
                 <Image
+                    {...props}
                     className={styles.logo}
-                    src="/istockphoto-1188966913-612x612.jpg"
-                    alt="Next.js logo"
-                    width={612}
-                    height={612}
                     priority
                 />
             </main>
