9b21c461889dbde415920a7185471054686df780
Lucas M. Oromi <lucas.oromi@gmail.com>
2025-06-10 10:02:58 -0600

    bird

--- petblack/src/app/page.tsx
+++ petblack/src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 
 const images = [
     {alt: 'dog', src: '/istockphoto-1188966913-612x612.jpg', height: 612, width: 612},
-    // {alt: 'bird', src: '/photo-1709439681846-60a076f48892.jpg', height: 4499, width: 3000},
+    {alt: 'bird', src: '/photo-1709439681846-60a076f48892_small.jpg', height: 720, width: 480},
     {alt: 'cat', src: '/blackcat_Lifeonwhite_Alamy_Stock_Photo.jpeg', height: 855, width: 656},
     {alt: 'fish', src: '/117919784-black-goldfish-isolated-on-background.jpg', height: 450, width: 450},
     // {alt: 'dog and cat', src: '/360_F_316422277_s4yn3aOAojJnQPZE1SfKOFIPvSKqCxjv.jpg', height: 360, width: 548}
